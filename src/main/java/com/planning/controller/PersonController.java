8b72f9150be3b4023eecf8c039ac76e10edf155a
xingcheng.yin <Bepatient@123>
2020-02-24 14:35:17 +0800

    init project

---
+++ src/main/java/com/planning/controller/PersonController.java
@@ -0,0 +1,11 @@
+package com.planning.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/person")
+public class PersonController {
+
+
+}
